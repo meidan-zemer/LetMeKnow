9e47b7d304c5538fb356d9818aa779ee1d1a4f7d
meidan.zemer <meidan@gmail.com>
2018-12-29 17:54:33 +0200

    First commit

---
+++ src/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text} from "react-native";
+
+
+interface Props {}
+export default class App extends Component<Props> {
+    render() {
+        return (
+               <Text >My first Comp$$$$$$$$$!!!!!!!</Text>
+        );
+    }
+}
