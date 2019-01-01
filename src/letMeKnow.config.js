04c1dc8d07172fcf9f7d75c086ff4f83f527d129
meidan.zemer <meidan@gmail.com>
2019-01-01 21:22:52 +0200

    change server base url

--- src/letMeKnow.config.js
+++ src/letMeKnow.config.js
@@ -1,6 +1,6 @@
 const config = {
   axiosClient: {
-    baseURL: 'https://m2v6s60dx8.execute-api.us-east-1.amazonaws.com/dev',
+    baseURL: 'https://5i7o1rkf6l.execute-api.eu-west-1.amazonaws.com/dev/',
     responseType: 'json'
   }
 };
